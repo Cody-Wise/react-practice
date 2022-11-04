f08b259815db9cc145c87e96dfeb30bed7674518
Cody-Wise <cody@sharptackmedia.com>
2022-11-03 18:29:09 -0700

    First challenge done

---
+++ demoapp/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='square'>
+        <p>Hello World</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
