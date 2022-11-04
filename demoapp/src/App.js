824cae75c2c51761fd1bb435c2bfc0cb1b70c084
Cody-Wise <cody@sharptackmedia.com>
2022-11-03 18:33:20 -0700

    Finished

--- demoapp/src/App.js
+++ demoapp/src/App.js
@@ -2,10 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function handleClick() {
+    alert('Clicked');
+  }
   return (
     <div className='App'>
       <div className='square'>
         <p>Hello World</p>
+        <button onClick={handleClick}>Click Me!</button>
       </div>
     </div>
   );
